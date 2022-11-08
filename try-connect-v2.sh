2de4799ba3860cb9211fe03857d8c7828e6ce017
karin0 <karin0@gmx.com>
2022-11-08 17:55:39 +0800

    try-connect with cookie.jar

--- try-connect-v2.sh
+++ try-connect-v2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-online=$(curl -sS "https://gw.buaa.edu.cn/cgi-bin/rad_user_info")
-grep "not_online_error" <<< $online > /dev/null
-if [[ "$?" != "0" ]]; then
-        echo online: $(cut -d, -f1 <<< $online)
-        exit 1
+online=$(
+curl -sSL "https://gw.buaa.edu.cn/cgi-bin/rad_user_info" \
+-c cookie.jar \
+--header "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36" \
+)
+
+echo "Gateway: $online"
+if grep -q "not_online_error" <<< $online; then
+  bash ./login-v2.sh login
+else
+  echo online: $(cut -d, -f1 <<< $online)
 fi
-
-./login-v2.sh login
-
